fa4d9ead7efdcd6f9de846821980b61f9f5713bf
Kevin Park <keunsuk413@gmail.com>
2019-12-06 16:23:07 -0600

    added Chris' style of prettier

--- test.js
+++ test.js
@@ -1,21 +1,24 @@
 $(function() {
-  $('#js-shopping-list-form').submit( event => {
+  $('#js-shopping-list-form').submit(event => {
     // stop the default form submission behavior
     event.preventDefault();
-    
-    const shoppingItem = $(event.currentTarget).find('input[name="shopping-list-entry"]').val();
+
+    const shoppingItem = $(event.currentTarget)
+      .find('input[name="shopping-list-entry"]')
+      .val();
     console.log(shoppingItem);
-    
+
     //add item in shopping list
-    $(".shopping-list").append(`<li><span class="shopping-item"> ${shoppingItem} </span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
-    
+    $('.shopping-list').append(
+      `<li><span class="shopping-item"> ${shoppingItem} </span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
+    );
 
     //remove items from shopping list
     $(document).on('click', 'shipping-item-delete', function() {
-      this.closest("li").remove(); //Both jQuery and Javascript have closest() function. In this case, both this or $(this) work.
+      this.closest('li').remove(); //Both jQuery and Javascript have closest() function. In this case, both this or $(this) work.
       //Find the parent "li" and remove it
     });
-    
+
     //write completely without jquery (in this case you don't need "$(function)......")
     /*
     document.addEventListener('click', function() {
@@ -27,10 +30,10 @@ $(function() {
 
     //Check items
     $(document).on('click', '.shopping-item-toggle', function() {
-      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-
+      $(this)
+        .closest('li')
+        .find('.shopping-item')
+        .toggleClass('shopping-item__checked');
     });
-
   });
-
 });
